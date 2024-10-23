0a7e6efd23b7664ebe7a1b4cb6d5c1b1b77a84ce
DAK-EE <157698851+DAK-EE@users.noreply.github.com>
2024-10-23 15:11:59 -0700

    new DHT
    Re-organized code for DHT11

--- HW507-DHT11/src/main.cpp
+++ HW507-DHT11/src/main.cpp
@@ -23,10 +23,6 @@
 *
 * Dependencies:
 *   include\ArduinoJson-v6.21.5.h
-*
-* 
-* 
-*
 * ---------------------------------------------------------------------------
 */
 
@@ -39,127 +35,126 @@
 #include "DHT.h"
 #include "Adafruit_Sensor.h"
  
+#define BUTTON_PIN D0
 #define DHT11_PIN 5
 DHT dht11(DHT11_PIN, DHT11);
 
 const char* ssid = "iPhone 190"; //Enter SSID
 const char* password = "myhotspot"; //Enter Password
 
+void setupWiFi();
 bool readButton();
-String getTime();
-int transmit(int temperature,int humidity,String time_acquired);
+String sendHTTP(String URL);
 String getZone();
+String getTime();
+String parseTime(String payload);
+void transmit(int temperature,int humidity,String time_acquired);
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) 
-  {
-     delay(500);
-     Serial.print("*");
-  }
-  Serial.println("");
-  Serial.println("WiFi connection Successful");
-  pinMode(D0, INPUT);
+  setupWiFi();
+  pinMode(BUTTON_PIN, INPUT);
   dht11.begin();
 }
 
 void loop() {
-  if(readButton()){
-    Serial.println("Button is pushed");
+  if(readButton()) {
     float humidity = dht11.readHumidity();
     float temperature = dht11.readTemperature(true);
     String time = getTime();
     transmit(temperature, humidity, time);
     delay(1000);
   }
-  delay(10);
+}
+
+void setupWiFi(){
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+     delay(500);
+     Serial.print("*");
+  }
+  Serial.println("");
+  Serial.println("WiFi connection Successful");
+  return;
 }
 
 bool readButton(){
-  return digitalRead(D0);
+  return digitalRead(BUTTON_PIN);
+}
+
+String sendHTTP(String URL){
+  String payload = "";
+  if(WiFi.status()==WL_CONNECTED) {
+      /*BearSSL client "clientS" is used for HTTPS requests, as opposed to HTTP requests*/
+      WiFiClientSecure clientS;
+      WiFiClient client;
+      HTTPClient http;
+      if(URL.charAt(4)=='s'){
+        Serial.println("**Establishing BearSSL WiFi Client**");
+        clientS.setInsecure();
+        http.begin(clientS, URL);
+      } else {
+        Serial.println("**Establishing Arduino WiFi Client**");
+        http.begin(client, URL);
+      }
+      int httpResponseCode = http.GET();
+      if(httpResponseCode > 0) {
+        payload = http.getString();
+        Serial.println("HTTP Response code: " + String(httpResponseCode));
+        Serial.println("Payload: " + payload);
+      } else {
+        Serial.print("Error on HTTP request: ");
+        Serial.println(httpResponseCode);
+      }
+      http.end();
+    } else {
+      Serial.println("WiFi connection could not be established.");
+    }
+  return payload;
+};
+
+String getZone(){
+  String payload = sendHTTP("http://67.169.107.53/timezone.php");
+  return payload;
 }
 
 String getTime() {
-  WiFiClientSecure client;
-  client.setInsecure();
-  HTTPClient https;
   String zone = getZone();
   String URL = "https://timeapi.io/api/time/current/zone?timeZone=";
   URL = URL + zone;
-  if(https.begin(client, URL)){
-    int HTTPCode = https.GET();
-    if(HTTPCode > 0){
-      String time = https.getString();
-      DynamicJsonDocument doc(ESP.getMaxFreeBlockSize() - 512);
-      deserializeJson(doc, time);
-      String yyyy = doc["year"];
-      String mm = doc["month"];
-      String dd = doc["day"];
-      String H = doc["hour"];
-      String i = doc["minute"];
-      String s = doc["seconds"];
-      if (s.length()==1){
-        s = "0" + s;
-      }
-      String mytime = yyyy + "-" + mm + "-" + dd + "%20" + H + "%3A" + i + "%3A" + s;
-      https.end();
-      Serial.println(mytime);
-      return mytime;
-      }
-    https.end(); 
-  } else {
-      Serial.println("Unable to connect using HTTPS.");
-  }
-  return "Unable to connect using HTTPS.";
+  String payload = sendHTTP(URL);
+  String time = parseTime(payload);
+  return time;
 }
 
-int transmit(int temperature, int humidity, String time_acquired){
+String parseTime(String payload){
+  DynamicJsonDocument doc(ESP.getMaxFreeBlockSize() - 512);
+  deserializeJson(doc, payload);
+  String yyyy = doc["year"];
+  String mm = doc["month"];
+  String dd = doc["day"];
+  String H = doc["hour"];
+  String i = doc["minute"];
+  String s = doc["seconds"];
+  if (s.length()==1){
+    s = "0" + s;
+  }
+  String mytime;
+  mytime = yyyy + "-" + mm + "-" + dd + "%20" + H + "%3A" + i + "%3A" + s;
+  return mytime;
+}
+
+void transmit(int temperature, int humidity, String time_acquired){
   Serial.println(temperature);
   Serial.println(humidity);
   Serial.println(time_acquired);
+  
   String URL = "http://67.169.107.53/DHT11submit.php?node_name=node_1&temperature=";
   URL = URL + temperature;
   URL = URL + "&humidity=";
   URL = URL + humidity;
   URL = URL + "&time_acquired=";
   URL = URL + time_acquired;
-  if(WiFi.status()==WL_CONNECTED) {
-    WiFiClient client;
-    HTTPClient http;
-    http.begin(client, URL);
-    int httpResponseCode = http.GET();
-    if(httpResponseCode > 0) {
-      String payload = http.getString();
-      Serial.println("HTTP Response code: " + String(httpResponseCode));
-      Serial.println("Payload: " + payload);
-    } else {
-      Serial.print("Error on HTTP request: ");
-      Serial.println(httpResponseCode);
-    }
-    http.end();
-    delay(1000);
-  }
-  return 1;
-}
-
-String getZone(){
-    String payload;
-    if(WiFi.status()==WL_CONNECTED) {
-    WiFiClient client;
-    HTTPClient http;
-    http.begin(client, "http://67.169.107.53/timezone.php");
-    int httpResponseCode = http.GET();
-    if(httpResponseCode > 0) {
-      payload = http.getString();
-      Serial.println("HTTP Response code: " + String(httpResponseCode));
-      Serial.println("Payload: " + payload);
-    } else {
-      Serial.print("Error on HTTP request: ");
-      Serial.println(httpResponseCode);
-    }
-    http.end();
-    delay(1000);
-  }
-  return payload;
+  sendHTTP(URL);
+  return;
 }
